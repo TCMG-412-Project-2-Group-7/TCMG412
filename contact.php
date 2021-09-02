19748589fa6adcc47ee1edc587bc2b40fa987233
pshuck-code <66891908+pshuck-code@users.noreply.github.com>
2021-09-02 12:34:23 -0500

    PHP Final
    PHP Finally Done

--- contact.php
+++ contact.php
@@ -1,9 +1,7 @@
 <?php
-// database connection code
-//$con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-
 $con= mysqli_connect('127.0.0.1', 'root', '','db_connect');
 
+    
 // get the post records
 $txtName = $_POST['txtName'];
 $txtEmail = $_POST['txtEmail'];
@@ -11,7 +9,7 @@ $txtPhone = $_POST['txtPhone'];
 $txtMessage = $_POST['txtMessage'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_form` (`name`, `email`, `phone`, `message`) VALUES ($txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+$sql = "INSERT INTO `tbl_form` (`name`, `email`, `phone`, `message`) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
@@ -20,9 +18,13 @@ if($rs)
 {
 	echo "Contact Records Inserted";
 }
+else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
 if (!$con) {
 	die("Connection failed: " . mysqli_connect_error());
   }
+
 
 ?>
 
